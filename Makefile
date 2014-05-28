71869042f3e8c6574f54deefcd09029d8a7b4d70
root <root@ubuntu.(none)>
2014-05-28 02:11:56 -0700

    add test.cc and makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+target:
+	g++ -std=c++11 -Wall test.cc -o test
---
+++ test.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	constexpr int a = 0;
+	cout << a << endl;
+	return 0;
+}
